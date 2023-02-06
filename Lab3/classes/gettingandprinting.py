0d9d2ca75632a6333bbfdbf03002c15ae4c35291
Askar <askar4ik123@gmail.com>
2023-02-06 22:47:54 +0600

    finishing classes in python

---
+++ Lab3/classes/gettingandprinting.py
@@ -0,0 +1,11 @@
+class String:
+    def __init__(self):
+        self.userInput = None
+    def getString(self):
+        self.userInput = input()
+    def printString(self):   
+        print(self.userInput.upper())
+
+string = String()
+string.getString()
+string.printString()
